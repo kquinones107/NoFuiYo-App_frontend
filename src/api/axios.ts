7066568c61039ff5f341486a499e92d2db10ed25
kquinones107 <kquinones@unal.edu.co>
2025-05-25 00:57:00 -0500

    commit:25/05/2025

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://192.168.1.4:5000/api', // Reemplaza con tu IP local real
+  baseURL: 'https://nofuiyoapp-backend.onrender.com/api', // Reemplaza con tu IP local real
 });
 
 export default API;
